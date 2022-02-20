d545513750fb5b8f210140cb6122ebc48d36a850
nikolay <n_yozov@hotmail.com>
2022-02-19 22:24:10 -0500

    add to projects section, add skills section

--- src/components/NameBox.jsx
+++ src/components/NameBox.jsx
@@ -3,7 +3,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function NameBox() {
   return (
-    <div className="relative dark:bg-gray-900 overflow-hidden">
+    <div className="h-screen relative dark:bg-gray-900 overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 dark:bg-gray-900 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <main className="dark:bg-gray-900 mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,83 @@
-import React from 'react'
+import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
+import etuneImage from '../assets/etune_image.png'
+import therapifyImage from '../assets/therapify_image.png'
 
-export default function Projects() {
+const features = [
+  {
+    name: 'Front End',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: GlobeAltIcon,
+  },
+  {
+    name: 'Back End',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ScaleIcon,
+  },
+]
+
+export default function Example() {
   return (
-    <div className="relative dark:bg-gray-900 overflow-hidden">
-    <div className="max-w-7xl mx-auto">
-      <div className="relative z-10 pb-8 dark:bg-gray-900 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-     
+    <div className="py-12 dark:bg-gray-900 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h1 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Recent Projects</h1>
+         
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
+            accusamus quisquam.
+          </p>
+        </div>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
+                                    <div>
+                                        <img className="w-full" src={etuneImage} alt="games" />
+                                        <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
+                                            <p className="text-sm text-white font-semibold tracking-wide">Etune</p>
+                                            <p className="text-sm text-white font-semibold tracking-wide">React, Express</p>
+                                            
+                                        </div>
+                                        <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                                           
+                                            
+                                            <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">Find the latest events updates or create events, concerts, conferences, workshops...</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <img className="w-full" src={therapifyImage} alt="notes" />
+                                        <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
+                                            <p className="text-sm text-white font-semibold tracking-wide">Therapify</p>
+                                            <p className="text-sm text-white font-semibold tracking-wide">React Express</p>
+                                        </div>
+                                        <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                              
+                                            <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">Find the latest events updates or create events, concerts, conferences, workshops...</p>
+                                        </div>
+                                    </div>
+                                </div>
 
-       
-
-        <main className="dark:bg-gray-900 mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-          <div className="sm:text-center dark:bg-gray-900 lg:text-left">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="dark:text-white block xl:inline">Recent Projects</span>{' '}
-             
-            </h1>
-            
-            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-              
-             
-            </div>
-            
-          </div>
-        </main>
+        <div className="mt-10">
+        <div className="lg:text-center">
+          <h1 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Skills</h1>
+         
+          
+        </div>
+          <dl className="mt-4 space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+          
+            {features.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium dark:text-white text-gray-900">{feature.name}</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
       </div>
     </div>
-    
-  </div>
   )
 }
